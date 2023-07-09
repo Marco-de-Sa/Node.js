f43db9c6c54144a0afcda3ed4c5a4e79de95e3bf
Marco-de-Sa <Marcojb.desa@gmail.com>
2023-07-09 13:32:46 +0200

    modified:   get-programming-with-nodejs/unit_2/lesson_11/start/express_templates/controllers/homeController.js 	modified:   get-programming-with-nodejs/unit_2/lesson_11/start/express_templates/package.json 	modified:   get-programming-with-nodejs/unit_2/lesson_11/start/express_templates/views/index.ejs 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/controllers/errorController.js 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/controllers/homeController.js 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/main.js 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/package.json 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/public/404.html 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/public/css/bootstrap.css 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/public/css/confetti_cuisine.css 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/public/images/cat.jpg 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/public/images/fruit.jpg 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/public/images/graph.png 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/public/images/icon.png 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/public/images/people.jpg 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/public/js/confettiCuisine.js 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/views/contact.ejs 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/views/courses.ejs 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/views/error.ejs 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/views/index.ejs 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/views/layout.ejs 	new file:   get-programming-with-nodejs/unit_2/lesson_12/start/confetti_cuisine/views/thanks.ejs

--- get-programming-with-nodejs/unit_2/lesson_11/start/express_templates/controllers/homeController.js
+++ get-programming-with-nodejs/unit_2/lesson_11/start/express_templates/controllers/homeController.js
@@ -1,10 +1,15 @@
 "use strict";
 
+exports.respondWithHome = (req, res) =>{
+  res.send("this is the home page for lesson 10")
+}
+
 exports.sendReqParam = (req, res) => {
   let veg = req.params.vegetable;
   res.send(`This is the page for ${veg}`);
 };
 
 exports.respondWithName = (req, res) => {
-  res.render("index");
+  let paramsName = req.params.myName;
+  res.render("index", { name: paramsName });
 };
